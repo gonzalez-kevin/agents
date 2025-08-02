748a404dace9d7a02f6fbd274df8028e14c35b87
gonzalez-kevin <keving12989@icloud.com>
2025-08-02 16:53:53 -0700

    reading file content

--- functions/get_files_info.py
+++ functions/get_files_info.py
@@ -1,8 +1,8 @@
 import os
+from config import MAX_CHARS
 
 def get_files_info(working_directory, directory = "."):
     full_path = os.path.join(working_directory, directory)
-    
     abs_working_dir = os.path.abspath(working_directory)
     abs_target_dir = os.path.abspath(full_path)
     
@@ -24,3 +24,24 @@ def get_files_info(working_directory, directory = "."):
         return "\n".join(files_info)
     except Exception as e:
         return f"Error: Error listing files: {e}"
+    
+    
+def get_file_content(working_directory, file_path):
+    full_path = os.path.join(working_directory, file_path)
+    abs_working_dir = os.path.abspath(working_directory)
+    abs_target_file = os.path.abspath(full_path)
+    
+    if not abs_target_file.startswith(abs_working_dir):
+        return f'Error: Cannot read "{file_path}" as it is outside the permitted working directory'
+    
+    if not os.path.isfile(abs_target_file):
+        return f'Error: File not found or is not a regular file: "{file_path}"'
+    try:
+        with open(abs_target_file, "r") as f:
+            file_content_string = f.read(MAX_CHARS)
+            if len(f.read()) > 10000:
+                file_content_string = file_content_string + f'[...File "{file_path}" truncated at 10000 characters]'
+                
+            return file_content_string
+    except Exception as e:
+        return f"Error: Error reading file: {e}"
--- tests.py
+++ tests.py
@@ -1,21 +1,21 @@
-from functions.get_files_info import get_files_info
+from functions.get_files_info import get_files_info, get_file_content
 
 def test():
-    result = get_files_info("calculator", ".")
+    result = get_file_content("calculator", "main.py")
     print("Result for current directory:")
     print(result)
     print("")
     
-    result = get_files_info("calculator", "pkg")
-    print("Result for 'pkg' directory:")
+    result = get_file_content("calculator", "pkg/calculator.py")
+    print("Result for current directory:")
     print(result)
     
-    result = get_files_info("caluclator", "/bin")
-    print("Result for '/bin' directory:")
+    result = get_file_content("calculator", "/bin/cat")
+    print("Result for current directory:")
     print(result)
     
-    result = get_files_info("calculator", "../")
-    print("Result for '../' directory:")
+    result = get_file_content("calculator", "pkg/does_not_exist.py")
+    print("Result for current directory:")
     print(result)
     
 if __name__ == "__main__":
