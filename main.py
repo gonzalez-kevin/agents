9d961a943dce499f4c75332debcd4bd853b36f82
gonzalez-kevin <keving12989@icloud.com>
2025-08-06 20:58:53 -0700

    agent calling function

--- main.py
+++ main.py
@@ -1,9 +1,12 @@
 import os
+import types
 from dotenv import load_dotenv
 from google import genai
 from google.genai import types
 import sys
 from config import *
+from functions.get_files_info import schema_get_files_info
+
 
 load_dotenv()
 api_key = os.environ.get("GEMINI_API_KEY")
@@ -19,18 +22,22 @@ def main():
     verbose = "--verbose" in sys.argv[2:]
     messages = [types.Content(role = "User", parts = [types.Part(text = user_prompt)]),]
 
-    content = client.models.generate_content(model = "gemini-2.0-flash-001", contents = messages, config = types.GenerateContentConfig(system_instruction = system_prompt))
+    available_functions = types.Tool(function_declarations = [schema_get_files_info])
+    content = client.models.generate_content(model = "gemini-2.0-flash-001", contents = messages, 
+                                            config = types.GenerateContentConfig(tools = [available_functions], system_instruction = system_prompt))
 
     prompt = content.usage_metadata.prompt_token_count
     candidate = content.usage_metadata.candidates_token_count
     
+    if content.function_calls:
+        print(f"Calling function: {content.function_calls[0].name}({content.function_calls[0].args})")
+
     if verbose:
-        print(f"User prompt: {user_prompt}")
-        print(f"Prompt tokens: {prompt}")
-        print(f"Response tokens: {candidate}")
-        print(content.text)
-    else:
-        print(content.text)
+        print(f'User prompt: {user_prompt}')
+        print(f'Prompt tokens: {prompt}')
+        print(f'Response tokens: {candidate}')
+
+    print(content.text)
 
 
 if __name__ == "__main__":
